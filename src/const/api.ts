887ac05b66c5af9b037ac5ee37bdf6cad321945d
이현종 <ehdclr@gractor.com>
2025-11-17 14:25:50 +0900

    feat: url endpoint const 추가

---
+++ src/const/api.ts
@@ -0,0 +1,12 @@
+export const API_ROUTES = {
+  AUTH: {
+    REFRESH: {
+      url: '/auth/refresh',
+      method: 'POST',
+    },
+    LOGOUT: {
+      url: '/auth/logout',
+      method: 'POST',
+    }
+  }
+}
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,31 +1,54 @@
 import axios from 'axios';
 
-console.log('API_URL', import.meta.env.VITE_API_URL);
-
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  withCredentials: true,
 });
 
 apiClient.interceptors.request.use((config) => {
-  // const token = localStorage.getItem('token');
-  // if (token) {
-  //   config.headers.Authorization = `Bearer ${token}`;
-  // }
+   if(!(config.headers['Content-Type'] === 'application/json')) {
+    config.headers['Content-Type'] = 'application/json';
+   } else {
+    //TODO: form-data 처리
+    delete config.headers['Content-Type'];
+   }
   return config;
 }, (error) => {
   return Promise.reject(error);
 });
 
-apiClient.interceptors.response.use((response) => {
-  return response;
-}, (error) => {
-  if(error.response.status === 401) {
-    console.error('Unauthorized');
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.data?.error === "Unauthorized") {
+      const refreshToken = error.config.headers["X-Refresh-Token"];
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+
+      try {
+        // 직접 서버에 리프레시 요청
+        const response = await axios.post(
+          `${SERVER_API_URL}/auth/refresh`,
+          { refreshToken },
+          {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+          }
+        );
+
+        const { accessToken } = response.data.data;
+        document.cookie = `accessToken=${accessToken}; path=/; maxAge=${15 * 60 * 1000}`; //15분 후 만료
+
+        return apiClient(error.config);
+      } catch (refreshError) {
+        window.location.href = "/signin";
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
+);
 
 export default apiClient;
